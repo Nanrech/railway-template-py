1797aba4390a55571d754946c748d18d3c27b3ae
Nan R <88463477+Nanrech@users.noreply.github.com>
2022-06-26 18:07:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 
 load_dotenv() # Boilerplate
-
-
 __SUPER_SECRET_STUFF = getenv("VENV_NAME") # Of course, you have to take into account the path between main.py and .env
 
 print(__SUPER_SECRET_STUFF) # Tada 🎉
